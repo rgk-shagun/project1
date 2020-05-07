f77a4215bb9b87d522fe49787e13c2a504c2c264
root <root@localhost.localdomain>
2020-05-07 02:01:43 -0400

    Python scripts

---
+++ addition.py
@@ -0,0 +1,22 @@
+print("****************************************************")
+print("********************                ****************")
+print("********************   FRUIT PRICE  ****************")
+print("********************                ****************")
+print("****************************************************")
+'''
+apple=80
+orange=35
+mango=75
+total=apple+orange+mango
+print(f"Price of apple {apple},orange {orange},mango {mango}\n total bill is : {total}")
+'''
+apple=eval(input("Enter apple price : "))
+orange=eval(input("Enter oracnge price : "))
+mango=eval(input("Enter mango price : "))
+total=apple+orange+mango
+print("              ")
+
+print(f"apple price is : {apple}")
+print(f"orange price is: {orange}")
+print(f"mango price is: {mango}")
+print(f"Total Bill is : {total}")
---
+++ test.py
@@ -0,0 +1,20 @@
+#/usr/local/bin/python3
+'''
+x=5
+y=9
+z="parmod kumar"
+len_z=len(z)
+#print(f"X value is : {x} Y value is : {y} Z value is {z}")
+print(f"X Value is : {x} \n Y value is : {y}\n Z value is : {(z.upper())}")
+print(f"{len_z}")
+result=x+y
+print(f"Addition of X & Y is : {result}")
+print
+'''
+
+my_name="Parmod Kumar Kushwaha"
+my_name_len=len(my_name)
+print(my_name[0:])
+print(my_name[:-1])
+print(my_name[-5:-1])
+print(my_name_len)
